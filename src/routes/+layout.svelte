73aad592e6e04f282601bb033494cc6dbe01289a
Abel Semahegn <abel249733@gmail.com>
2025-12-30 10:32:01 -0600

    convex init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import { ModeWatcher } from 'mode-watcher';
+	import { PUBLIC_CONVEX_URL } from '$env/static/public';
+	import { setupConvex } from 'convex-svelte';
+	setupConvex(PUBLIC_CONVEX_URL);
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>InkTime</title>
+</svelte:head>
+<ModeWatcher />
+<main class="h-full w-full">
+	{@render children()}
+</main>
