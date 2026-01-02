dfb53c026c1e46681096eb575571c38868fb20f5
Abel Semahegn <abel249733@gmail.com>
2026-01-02 11:15:19 -0600

    test

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -5,14 +5,26 @@ import { getRequestEvent } from '$app/server';
 import { env } from '$env/dynamic/private';
 import { db } from './db';
 export const auth = betterAuth({
+  advanced: {
+          crossSubDomainCookies: {
+              enabled: true,
+              domain: "." + new URL(env.BETTER_AUTH_URL).hostname
+          },
+          defaultCookieAttributes: {
+              secure: true,
+              httpOnly: true,
+              sameSite: "none",  // Allows CORS-based cookie sharing across subdomains
+              partitioned: true, // New browser standards will mandate this for foreign cookies
+          },
+      },
+      trustedOrigins: [
+        env.BETTER_AUTH_URL
+      ],
 	secret: env.BETTER_AUTH_SECRET!,
-
 	baseURL: env.BETTER_AUTH_URL,
-
 	database: drizzleAdapter(db, {
 		provider: 'pg' // or "mysql", "sqlite"
 	}),
-
 	plugins: [sveltekitCookies(getRequestEvent)], // make sure this is the last plugin in the array
 	emailAndPassword: {
 		enabled: true
