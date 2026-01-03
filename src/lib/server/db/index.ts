c760186896fbeb0dd5992640d28df948bc5a7c3a
Abel Semahegn <abel249733@gmail.com>
2026-01-03 17:09:45 -0600

    init will redo the ui so its a little clean and use username password system

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
-import * as schema from './schema/auth-schema';
+import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
