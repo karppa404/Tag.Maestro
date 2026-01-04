21ce0dc3be293eac0bca08fa83e75b487ccca09b
Abel Semahegn <abel249733@gmail.com>
2026-01-04 10:16:11 -0600

    ok so now it builds

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -3,8 +3,10 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
 import { sveltekitCookies } from "better-auth/svelte-kit";
 import { getRequestEvent } from "$app/server";
+import { env } from '$env/dynamic/private';
 
 export const auth = betterAuth({
+    secret: env.BETTER_AUTH_SECRET,
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
     }),
