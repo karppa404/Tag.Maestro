12daf659551587c60f69b2ee3d2d3795019dae64
Abel Semahegn <abel249733@gmail.com>
2026-01-08 19:04:00 -0600

    merge and shit

--- src/lib/client/auth-client.ts
+++ src/lib/client/auth-client.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createAuthClient } from "better-auth/svelte"
 import { env } from "$env/dynamic/public"
 import { anonymousClient } from "better-auth/client/plugins"
@@ -10,29 +9,3 @@ export const authClient = createAuthClient({
         anonymousClient()
     ]
 })
-=======
-import { createAuthClient } from 'better-auth/svelte';
-import { usernameClient } from 'better-auth/client/plugins';
-
-export const authClient = createAuthClient({
-	plugins: [usernameClient()]
-});
-
-// export async function signInWithTwitter() {
-// 	await authClient.signIn.social({
-// 		provider: 'twitter',
-// 		callbackURL: '/api/auth/callback/twitter',
-// 		errorCallbackURL: '/'
-// 	});
-// }
-export async function signInWitGithub() {
-	await authClient.signIn.social({
-		provider: 'github'
-	});
-}
-export async function signInWithGoogle() {
-	await authClient.signIn.social({
-		provider: 'google'
-	});
-}
->>>>>>> a9a9c569f62f5bad8871a9553677cee69bffed71
