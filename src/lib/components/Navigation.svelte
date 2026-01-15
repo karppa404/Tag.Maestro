86e956824a20d42d8fabd35adfcc1125f59f939e
Abel Semahegn <abel249733@gmail.com>
2026-01-15 15:38:38 -0600

    layout work still going strong will move on to convex setup

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { Input } from "$lib/components/ui/input/index.js";
+
+</script>
+
+<nav class="min-h-12 w-full p-2 max-h-12">
+    <Input type="search" placeholder="search" class="max-w-md" />
+
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import * as Resizable from "$lib/components/ui/resizable/index.js";
+	import { ModeWatcher } from "mode-watcher";
+import Navigation from '@/components/Navigation.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<ModeWatcher />
+<main class="w-full min-h-screen max-h-screen flex flex-col ">
+    <Navigation/>
+    <Resizable.PaneGroup direction="horizontal" class="flex-1 w-full h-full" >
+     
+      <Resizable.Pane defaultSize={75} minSize={60}>{@render children()}</Resizable.Pane>
+      <Resizable.Handle withHandle />
+      <Resizable.Pane>Two</Resizable.Pane>
+    </Resizable.PaneGroup>
+    
+</main>
