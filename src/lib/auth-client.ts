1ee8583d0fefee66ce7ea7786b885a7eb4a670e2
Abel Semahegn <abel249733@gmail.com>
2026-01-16 22:10:59 -0600

    init

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,3 @@
-import { createAuthClient } from 'better-auth/svelte';
-import { convexClient } from "@convex-dev/better-auth/client/plugins";
+import { createAuthClient } from 'better-auth/react'
 
-export const authClient = createAuthClient({
-  plugins: [convexClient()],
-});
+export const authClient = createAuthClient()
--- src/lib/components/Navigation.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { Input } from "$lib/components/ui/input/index.js";
-
-</script>
-
-<nav class="min-h-12 w-full p-2 max-h-12 border-b inline-flex items-center">
-    <Input type="search" placeholder="search" class="max-w-md" />
-
-</nav>
