a2f65efdd5059bfb0ea04ff6be3dd1edca8c8d00
Abel Semahegn <abel249733@gmail.com>
2026-01-26 20:34:43 -0600

    start of tagging functions

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Input from "./ui/input/input.svelte";
 	import ThemeToggle from "$lib/components/theme-toggle.svelte"
+	import {Button} from "$lib/components/ui/button"
 </script>
 
 <nav class="w-full min-h-12 border-b border-1/30 bg-muted inline-flex items-center p-4 gap-2">
@@ -9,5 +10,11 @@
         placeholder="search" 
         class="max-w-xs"
     />
+    <Button >
+        Select Tags
+    </Button>
+    <Button variant="secondary">
+        Edit Tags    
+    </Button>
     <ThemeToggle/>
 </nav>
