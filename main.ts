9577fd21601af39821740d4afb19ec6d9acb8dda
Abel Semahegn <abel249733@gmail.com>
2026-01-30 10:35:16 -0600

    electron dev enviorment works now

--- main.ts
+++ main.ts
@@ -3,12 +3,26 @@ import { app, BrowserWindow } from 'electron';
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1280,
-    height: 720
-  })
+    height: 720,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
 
-  win.loadFile('./src/app.html')
-}
+  // Load from Vite dev server
+  win.loadURL('http://localhost:3000');
+  
+  // Open DevTools in development
+  win.webContents.openDevTools();
+};
 
 app.on('ready', () => {
-  createWindow()
-})
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
