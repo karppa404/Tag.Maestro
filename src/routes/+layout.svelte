e6eaa9165273d057156824e9d286803739f984b8
Abel Semahegn <abel249733@gmail.com>
2026-01-30 11:15:58 -0600

    shadcn svelte & theme toggle and theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import { ModeWatcher } from "mode-watcher";
+	import ThemeToggle from '@/components/ThemeToggle.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<ModeWatcher />
+<ThemeToggle/>
 {@render children()}
