0fbbab809c6aeb70538cbc68e288d521ab3a36e5
Abel Semahegn <abel249733@gmail.com>
2026-02-05 14:16:51 -0600

    will move on to making sure this shit builds

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   preprocess: vitePreprocess(),
   kit: {
+    alias: {
+      "@/*": "./src/lib/*",
+    },
     adapter: adapter({
       pages: 'build',
       assets: 'build',
